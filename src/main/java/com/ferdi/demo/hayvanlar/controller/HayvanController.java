e9f56dd14c4ad140fa0edfa3ce6a8b196efcc91a
ferdi yondemli <ferdi.yondemli@hmb.gov.tr>
2023-02-13 11:34:15 +0300

    little update

--- src/main/java/com/ferdi/demo/hayvanlar/controller/HayvanController.java
+++ src/main/java/com/ferdi/demo/hayvanlar/controller/HayvanController.java
@@ -1,6 +1,7 @@
 package com.ferdi.demo.hayvanlar.controller;
 
 import com.ferdi.demo.hayvanlar.controller.dto.HayvanResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class HayvanController {
 
 
     @GetMapping("/getirhayvan")
+    @Operation(description = "Hayvan Bilgisini Getirir.")
     public HayvanResponseDTO getirHayvan(){
 
         return new HayvanResponseDTO();
